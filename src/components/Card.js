298ff803a7adce68adc0ee7eba79d50f288e7d43
FuFuUssh <102244060+FuFuUssh@users.noreply.github.com>
2022-03-25 23:14:13 +0700

    Add files via upload

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,7 @@ import Modal from './Modal'
 
 export default function Card({ soundId, name, examples, links}) {
     const [openModal, setOpenModal] = useState(false)
+    const { lettersToMark, words } = examples
 
     function handleCardClick(e) {
         if (e.target.closest('.ipa-card-container') != null) {
@@ -16,14 +17,45 @@ export default function Card({ soundId, name, examples, links}) {
         setOpenModal(false)
     }
 
+    words.forEach(word => {
+        const letters = word.split('')
+        console.log(letters)
+    })
+
     return (
         <div className="ipa-card-container" onClick={handleCardClick}>
             <div className="ipa-card card">
                 <div className="card-front">/{name}/</div>
                 <div className="card-back">
-                    {examples?.map((example, index) => {
-                        return <p className="card-example" key={`example_${index}`}>{example}</p>
-                    })}
+                    <div className="examples-container">
+                        {words?.map((word, wordIndex) => {
+                        const letters = word.split('')
+                            return <div className="word">
+                                {letters.map((letter, index) => {
+                                    if (
+                                            lettersToMark.includes(letter) 
+                                            || lettersToMark.includes(word.substring(index, index + 2)) 
+                                            || lettersToMark.includes(word.substring(index - 1, index + 1))) {
+                                        return (
+                                            <>
+                                                {wordIndex !== 0 && index === 0 && <span>, </span>}
+                                                <span key={`letter_mark_${word}_${index}`} className="marked">
+                                                    {letter}
+                                                </span>
+                                            </>)
+                                    } else {
+                                        return (
+                                        <>
+                                            {wordIndex !== 0 && index === 0 && <span>, </span>}
+                                            <span key={`letter_mark_${word}_${index}`}>
+                                                {letter}
+                                            </span>
+                                        </>)
+                                    }
+                                })}
+                            </div>
+                        })}
+                    </div>
                 </div>
             </div>
             {openModal && ReactDOM.createPortal(
